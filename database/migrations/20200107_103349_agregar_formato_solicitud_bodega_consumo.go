4e09e519ff92fe5595c6f0a5156c86f736749b3b
ManuelMurillo <jmmurilloy@correo.udistrital.edu.co>
2020-01-07 10:53:50 -0500

    formato solicitud Elementos Bodega de consumo

---
+++ database/migrations/20200107_103349_agregar_formato_solicitud_bodega_consumo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type AgregarFormatoSolicitudBodegaConsumo_20200107_103349 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &AgregarFormatoSolicitudBodegaConsumo_20200107_103349{}
+	m.Created = "20200107_103349"
+
+	migration.Register("AgregarFormatoSolicitudBodegaConsumo_20200107_103349", m)
+}
+
+// Run the migrations
+func (m *AgregarFormatoSolicitudBodegaConsumo_20200107_103349) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("INSERT INTO movimientos_arka.formato_tipo_movimiento (nombre, formato, descripcion, codigo_abreviacion, numero_orden, fecha_creacion, fecha_modificacion, activo) VALUES ('Solicitud Elementos Bodega de Consumo', '{ \"Elementos\": [ { \"Funcionario\": \"number\", \"Ubicacion\": \"number\", \"ElementoActa\": \"number\", \"Cantidad\": \"number\" } ] }', 'Formato para guardar una solicitud de Bodega de Consumo', 'SOL_BOD', 8.0, now(), now(), true);")
+
+}
+
+// Reverse the migrations
+func (m *AgregarFormatoSolicitudBodegaConsumo_20200107_103349) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("DELETE FROM movimientos_arka.estado_movimiento WHERE codigo_abreviacion = 'SOL_BOD';")
+
+}
