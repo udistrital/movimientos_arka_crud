5421800b67aa4fb13a023efffbc742752ea86ec0
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-09-27 23:26:22 -0500

    feat udistrital/arka_cliente#577: Se cambia condición para el where en el update, delete del script down de la migración

--- database/scripts/20210925_182821_estados_salidas_down.sql
+++ database/scripts/20210925_182821_estados_salidas_down.sql
@@ -2,6 +2,6 @@ UPDATE movimientos_arka.estado_movimiento SET
     nombre = 'Salida Aceptada',
     descripcion = 'Formato para marcar una salida como aceptada/aprobada',
     fecha_modificacion = now()
-WHERE id = 3;
+WHERE nombre = 'Salida En Trámite';
 
-DELETE FROM movimientos_arka.estado_movimiento WHERE id = 9;
+DELETE FROM movimientos_arka.estado_movimiento WHERE nombre = 'Salida Aprobada';
--- database/scripts/20210925_182821_estados_salidas_up.sql
+++ database/scripts/20210925_182821_estados_salidas_up.sql
@@ -2,17 +2,15 @@ UPDATE movimientos_arka.estado_movimiento SET
     nombre = 'Salida En Trámite',
     descripcion = 'Salida en estado de trámite',
     fecha_modificacion = now()
-WHERE id = 3;
+WHERE nombre = 'Salida Aceptada';
 
 INSERT INTO movimientos_arka.estado_movimiento (
-    id,
     nombre,
     descripcion,
     activo,
     fecha_modificacion,
     fecha_creacion)
 VALUES (
-    9,
     'Salida Aprobada',
     'Salida en estado aprobada',
     true,
