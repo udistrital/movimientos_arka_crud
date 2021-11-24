12152fce18f29d3940ed374461d99c04c5a37a94
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2021-11-24 15:55:17 -0500

    feat udistrital/arka_cliente#843: Traslados
    - Query up ajustado una vez la migracion en ambiente de pruebas fue ejecutada

--- database/scripts/20211118_122319_funcionalidad_traslados_up.sql
+++ database/scripts/20211118_122319_funcionalidad_traslados_up.sql
@@ -1,42 +1,42 @@
--- UPDATE movimientos_arka.formato_tipo_movimiento SET
---     nombre = 'Traslado',
---     descripcion = 'Traslado de bien',
---     formato = '{ "Elementos": [], "Ubicacion": "number", "Consecutivo": "string", "FuncionarioOrigen": "number" , "FuncionarioDestino": "number" }',
---     fecha_modificacion = now()
--- WHERE nombre = 'Solicitud de Traslado';
+UPDATE movimientos_arka.formato_tipo_movimiento SET
+    nombre = 'Traslado',
+    descripcion = 'Traslado de bien',
+    formato = '{ "Elementos": [], "Ubicacion": "number", "Consecutivo": "string", "FuncionarioOrigen": "number" , "FuncionarioDestino": "number" }',
+    fecha_modificacion = now()
+WHERE nombre = 'Solicitud de Traslado';
 
--- INSERT INTO movimientos_arka.estado_movimiento (
---     nombre,
---     descripcion,
---     activo,
---     fecha_modificacion,
---     fecha_creacion)
--- VALUES
---     ('Traslado Rechazado',
---     'Traslado rechazado por almacen',
---     true,
---     now(),
---     now()),
---     ('Traslado Aprobado',
---     'Traslado aprobado por almacén',
---     true,
---     now(),
---     now()),
---     ('Traslado En Trámite',
---     'Traslado solicitado',
---     true,
---     now(),
---     now()),
---     ('Traslado Confirmado',
---     'Traslado confirmado por funcionario destino',
---     true,
---     now(),
---     now()),
---     ('Registro Kardex',
---     'Movimiento de bodega de consumo',
---     true,
---     now(),
---     now());
+INSERT INTO movimientos_arka.estado_movimiento (
+    nombre,
+    descripcion,
+    activo,
+    fecha_modificacion,
+    fecha_creacion)
+VALUES
+    ('Traslado Rechazado',
+    'Traslado rechazado por almacen',
+    true,
+    now(),
+    now()),
+    ('Traslado Aprobado',
+    'Traslado aprobado por almacén',
+    true,
+    now(),
+    now()),
+    ('Traslado En Trámite',
+    'Traslado solicitado',
+    true,
+    now(),
+    now()),
+    ('Traslado Confirmado',
+    'Traslado confirmado por funcionario destino',
+    true,
+    now(),
+    now()),
+    ('Registro Kardex',
+    'Movimiento de bodega de consumo',
+    true,
+    now(),
+    now());
 
 ALTER TABLE movimientos_arka.movimiento
     ALTER COLUMN detalle TYPE jsonb;
