cc759aa44ed6441840fe3a272300d2cdf14cd34a
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2021-12-04 22:01:28 -0500

    feat udistrital/arka_cliente#844: Bajas
    - Controlador para actualizar varias solicitudes una vez se registra el resultado del comite de almacen

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,6 +7,15 @@ import (
 
 func init() {
 
+    beego.GlobalControllerRouter["github.com/udistrital/movimientos_arka_crud/controllers:BajasController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movimientos_arka_crud/controllers:BajasController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["github.com/udistrital/movimientos_arka_crud/controllers:ElementosMovimientoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movimientos_arka_crud/controllers:ElementosMovimientoController"],
         beego.ControllerComments{
             Method: "Post",
