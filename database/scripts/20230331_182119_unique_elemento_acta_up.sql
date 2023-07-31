e0c3cebfd66160e6e4cca0aeedd469b03c82dcda
lud <ludwinggsanchez@gmail.com>
2023-07-31 10:58:08 -0500

    feat udistrital/arka_cliente#1160: Despliegue bodega
    - Fix: Migración.

--- database/scripts/20230331_182119_unique_elemento_acta_up.sql
+++ database/scripts/20230331_182119_unique_elemento_acta_up.sql
@@ -1,15 +1,15 @@
-ALTER TABLE movimientos_arka.elementos_movimiento
-ALTER COLUMN elemento_acta_id DROP NOT NULL;
+-- ALTER TABLE movimientos_arka.elementos_movimiento
+-- ALTER COLUMN elemento_acta_id DROP NOT NULL;
 
-UPDATE movimientos_arka.elementos_movimiento
-SET elemento_acta_id = NULL
-WHERE elemento_acta_id = 0;
+-- UPDATE movimientos_arka.elementos_movimiento
+-- SET elemento_acta_id = NULL
+-- WHERE elemento_acta_id = 0;
 
-ALTER TABLE movimientos_arka.elementos_movimiento
-ADD CONSTRAINT IF NOT EXISTS uq_elemento_acta_id UNIQUE (elemento_acta_id),
-ADD CONSTRAINT IF NOT EXISTS ck_valor_total_valor_residual_elementos_movimiento CHECK(valor_total >= valor_residual),
-ADD CONSTRAINT IF NOT EXISTS ck_valor_residual_elementos_movimiento CHECK(valor_residual >= 0);
+-- ALTER TABLE movimientos_arka.elementos_movimiento
+-- ADD CONSTRAINT uq_elemento_acta_id UNIQUE (elemento_acta_id),
+-- ADD CONSTRAINT ck_valor_total_valor_residual_elementos_movimiento CHECK(valor_total >= valor_residual),
+-- ADD CONSTRAINT ck_valor_residual_elementos_movimiento CHECK(valor_residual >= 0);
 
-ALTER TABLE movimientos_arka.novedad_elemento
-ADD CONSTRAINT IF NOT EXISTS ck_valor_libros_valor_residual_novedad_elemento CHECK(valor_libros >= valor_residual),
-ADD CONSTRAINT IF NOT EXISTS ck_valor_residual_novedad_elemento CHECK(valor_residual >= 0);
+-- ALTER TABLE movimientos_arka.novedad_elemento
+-- ADD CONSTRAINT ck_valor_libros_valor_residual_novedad_elemento CHECK(valor_libros >= valor_residual),
+-- ADD CONSTRAINT ck_valor_residual_novedad_elemento CHECK(valor_residual >= 0);
